77da260a7a13cc2841705e40db1cfd1501e7989d
grfld3-bit <grfld3@gmail.com>
2025-09-29 16:11:11 +0700

    Create community.js

---
+++ pages/community.js
@@ -0,0 +1,10 @@
+export default function Community(){
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-2xl font-bold">🤝 Join the Bone Army</h1>
+      <p>Follow us and join the community:</p>
+      <a href="https://x.com/sskeletonk" target="_blank" className="text-blue-400">🐦 Twitter/X</a><br/>
+      <a href="https://t.me/sskeletonk" target="_blank" className="text-green-400">💬 Telegram</a>
+    </div>
+  )
+}
