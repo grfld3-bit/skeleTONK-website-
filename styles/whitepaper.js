7e1dbbb50a8f4e33ac6473a80c932a2a5409974a
grfld3-bit <grfld3@gmail.com>
2025-09-29 16:05:09 +0700

    Delete styles/whitepaper.js

--- styles/whitepaper.js
+++
@@ -1,9 +0,0 @@
-export default function Whitepaper(){
-  return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold">📜 Whitepaper - SkeleTONK</h1>
-      <p>Total Supply: 100 Trillion $TONK</p>
-      <p>Tokenomics: Buyback & Burn, Community Driven, Treasury Reserve.</p>
-    </div>
-  )
-}
