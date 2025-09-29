3e82e3f8c589b305d49f0c1ee8019c3ec427c4ef
grfld3-bit <grfld3@gmail.com>
2025-09-29 16:04:03 +0700

    Create whitepaper.js

---
+++ styles/whitepaper.js
@@ -0,0 +1,9 @@
+export default function Whitepaper(){
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">📜 Whitepaper - SkeleTONK</h1>
+      <p>Total Supply: 100 Trillion $TONK</p>
+      <p>Tokenomics: Buyback & Burn, Community Driven, Treasury Reserve.</p>
+    </div>
+  )
+}
